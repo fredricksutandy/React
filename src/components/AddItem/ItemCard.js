f1b3ceecff373a46e16f5904ec9042eade3fb003
fredricksutandy <53491566+fredricksutandy@users.noreply.github.com>
2023-12-26 21:17:37 +0700

    add item content order

--- src/components/AddItem/ItemCard.js
+++ src/components/AddItem/ItemCard.js
@@ -7,12 +7,14 @@ function ItemCard({ item, onDelete }) {
   };
 
   return (
-    <div className="item-card p-3 max-w-[300px] w-full min-w-[200px] bg-white shadow-sm">
-      <p className='font-bold text-xl text-blue-600'>${item.price}</p>
-      <h3 className="text-3xl font-bold mb-3">{item.title}</h3>
-      <p className='rounded-lg text-blue-800 border-blue-800 px-4 py-1 w-fit bg-blue-100 text-xs mb-3'>{item.category}</p>
-      <p className='text-gray-500'>{item.description}</p>
-      <button className='text-red-400 mt-3' onClick={handleDeleteClick}>Delete</button>
+    <div className="item-card max-w-[300px] w-full min-w-[200px] bg-white shadow-sm">
+      <p className='p-3 text-blue-800 w-fit bg-blue-100 text-xs w-full'>{item.category}</p>
+      <div className="p-3">
+        <h3 className="text-xl font-semibold mb-2 capitalize">{item.title}</h3>
+        <p className='text-gray-500 mb text-sm mb-4'>{item.description}</p>
+        <p className='font-bold text-3xl'>${item.price}</p>
+        <button className='text-red-400 mt-3' onClick={handleDeleteClick}>Delete</button>
+      </div>
     </div>
   );
 }
